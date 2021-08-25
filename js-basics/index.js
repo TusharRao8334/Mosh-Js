b934507221d746910902b4274510231f9231359c
Tushar Rajaram Rao <49467516+TusharRao8334@users.noreply.github.com>
2021-08-25 16:37:38 +0530

    js Basics part 1

---
+++ js-basics/index.js
@@ -0,0 +1,26 @@
+// // variable
+// let name = "Tushar",
+//   sirname = "Rao";
+// let age = 30;
+// let isApproved = false;
+// let education = null;
+
+// // constant
+// const interestRate = 6.3;
+// console.log(interestRate);
+
+// typeof operator
+
+// object
+
+let person = {
+  name: "Tushar",
+  age: 25,
+};
+
+// Dot notation
+console.log(person.name);
+
+// Bracket Notatio n
+person["name"] = "Mary";
+console.log(person.name);
