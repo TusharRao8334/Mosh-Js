ed871993aa7c211607fe663fe9a0dd4401879539
Tushar Rajaram Rao <49467516+TusharRao8334@users.noreply.github.com>
2021-08-31 20:50:25 +0530

    logical operator

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,21 +1,11 @@
-// operators
-// 1 Arithmetic
-let x = 4;
-let y = 5;
-console.log(x + y);
-console.log(x - y);
-console.log(x * y);
-console.log(x / y);
-console.log(y % x);
-console.log(x ** y);
-
-// increament
-console.log(x++);
-
-// decreament
-console.log(--x);
-
-// 2 Assignment
-// 3 Comparison
 // 4 Logical
+// logical AND (&&)
+
+let highIncome = false;
+let goodCibil = true;
+let eligibleForLoan = highIncome || goodCibil;
+console.log(eligibleForLoan);
+
+// logical OR (||)
+
 // 5 Bitwise
